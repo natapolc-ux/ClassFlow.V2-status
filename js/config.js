b18e6a3696b774c37ce3fc0d6a269a6d94847dbc
natapolc-ux <natapol.c@chs.ac.th>
2026-09-22 13:34:22 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // ใช้ Web App URL เดียวกับเว็บครูและเว็บนักเรียน
-const API_URL = 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwNY_ojt0kczg3zbcdypJ2g0C9HMm1R9agnzqGDl-O0Mm6h1Q3FjJ-V92uFb95eLtYFdw/exec';
